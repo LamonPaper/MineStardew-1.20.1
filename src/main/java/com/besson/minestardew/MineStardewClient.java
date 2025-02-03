039b8f3035dba875e16c72d667c8451f18575c2d
LamonPaper <1573115900@qq.com>
2025-02-04 00:53:39 +0800

    111

---
+++ src/main/java/com/besson/minestardew/MineStardewClient.java
@@ -0,0 +1,10 @@
+package com.besson.minestardew;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MineStardewClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
---
+++ src/main/java/com/besson/minestardew/MineStardewDataGenerator.java
@@ -0,0 +1,11 @@
+package com.besson.minestardew;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MineStardewDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
